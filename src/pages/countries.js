59eb249a00a27118dfd032bd91e93b3a01a03b93
Emily Maskin <emily.maskin@gmail.com>
2021-01-24 17:20:36 -0500

    revert node changes for now

--- src/pages/countries.js
+++ src/pages/countries.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import pp from 'papaparse';
 import Chart from '../components/Chart';
 import Layout from '../components/Layout';
+import { baseUrl } from '../utils/constants';
 import '../components/layout.css';
 
-const CountriesPage = ({ pageContext: { res } }) => (
-  <Layout>
-    {Object.keys(res)
-      .filter((key) => key !== 'World')
-      .map((c, index) => (
-        <Chart name={c} list={res} key={index} />
-      ))}
-  </Layout>
-);
+const CountriesPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const countryObj = {};
+    const getInitialData = async () => {
+      pp.parse(`${baseUrl}/vaccinations/vaccinations.csv`, {
+        header: true,
+        delimiter: ',',
+        worker: true,
+        download: true,
+        complete(results) {
+          results.data
+            .filter((r) => !!r.location)
+            .map((r) => {
+              if (countryObj[r.location]) {
+                countryObj[r.location].data.push(r);
+              } else {
+                countryObj[r.location] = { data: [r] };
+              }
+            });
+          setData(countryObj);
+        },
+      });
+    };
+
+    getInitialData();
+  }, []);
+
+  return (
+    <Layout>
+      {data &&
+        Object.keys(data)
+          .filter((key) => key !== 'World')
+          .map((c, index) => <Chart name={c} list={data} key={index} />)}
+    </Layout>
+  );
+};
 
 export default CountriesPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import pp from 'papaparse';
 import Chart from '../components/Chart';
 import Layout from '../components/Layout';
 import '../components/layout.css';
+import { baseUrl } from '../utils/constants';
 
-const IndexPage = ({ pageContext: { res } }) => (
-  <Layout>
-    <Chart name="World" list={res} large />
-  </Layout>
-);
+const IndexPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const countryObj = {};
+    const getInitialData = async () => {
+      pp.parse(`${baseUrl}/vaccinations/vaccinations.csv`, {
+        header: true,
+        delimiter: ',',
+        worker: true,
+        download: true,
+        complete(results) {
+          results.data
+            .filter((r) => !!r.location)
+            .map((r) => {
+              if (countryObj[r.location]) {
+                countryObj[r.location].data.push(r);
+              } else {
+                countryObj[r.location] = { data: [r] };
+              }
+            });
+          setData(countryObj);
+        },
+      });
+    };
+
+    getInitialData();
+  }, []);
+
+  return <Layout>{data && <Chart name="World" list={data} large />}</Layout>;
+};
 
 export default IndexPage;
